d3f6691899146a67c702fe98bb521a3c55a76d05
gabeswk <147556284+gabeswk@users.noreply.github.com>
2025-12-02 16:16:26 -0300

    1.0

--- src/main/java/Exec.java
+++ src/main/java/Exec.java
@@ -5,7 +5,7 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import java.awt.Font;
 
-public class exec {
+public class Exec {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Janela Hello");
@@ -15,11 +15,10 @@ public class exec {
             label.setFont(new Font("SansSerif", Font.PLAIN, 24));
 
             frame.getContentPane().add(label);
-            frame.setSize(1920, 1080);
+            frame.setSize(350, 150);
             frame.setResizable(false);
             frame.setLocationRelativeTo(null); // centraliza na tela
             frame.setVisible(true);
         });
     }
 }
-
